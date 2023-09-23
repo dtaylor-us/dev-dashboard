4b45799faaf3cb76192e4442316af6cadc5a34ed
dtaylor-us <derektaylor.us@gmail.com>
2023-09-22 23:17:34 -0500

    add prod config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiHost: 'http://api.dtaylor-us.info' // Production API host
+  apiHost: 'https://api.dtaylor-us.info' // Production API host
 };
